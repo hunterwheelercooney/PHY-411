9725961e23a9ed6fbf73a3b8e6cf03aa581a4afc
hunterwheelercooney <112420255+hunterwheelercooney@users.noreply.github.com>
2022-09-12 13:47:10 -0400

    Add files via upload

---
+++ Fibbonacci Code.py
@@ -0,0 +1,26 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+n1 = 0
+n2 = 1
+sum = n1 + n2
+
+
+while (sum < 10000) :
+    print(sum)
+    sum = n1 + n2
+    n1 = n2
+    n2 = sum
+    
+    
+  
+    
+    
+    
+    
